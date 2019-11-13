bbb1645c193b6c4a6f0db6080236bc34c409e22c
“ClarissaWaldron” <clarissa509@hotmail.com>
2019-11-13 15:16:38 +0000

    changes incl nav bar

--- src/App.js
+++ src/App.js
@@ -2,25 +2,28 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+
 function App() {
   return (
     <div className="App">
+      <nav>
+        <ul class="menu">
+          <li class="logo"><a href="#">Clarissa Waldron</a></li>
+          <li class="item"><a href="#">Home</a></li>
+          <li class="item"><a href="#">About me</a></li>
+          <li class="item"><a href="#">Skills</a></li>
+          <li class="item"><a href="#">Previous work</a></li>
+          <li class="item"><a href="#">Contact</a></li>
+        </ul>
+      </nav>
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Welcome to Clarissa's portfolio
+          <h1>Welcome to Clarissa's portfolio</h1>
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
     </div>
   );
 }
+
 
 export default App;
